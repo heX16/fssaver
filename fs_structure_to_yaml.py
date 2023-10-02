3ee65f1a61b71bc652ac702eaa2160fa11b1c360
heX <hex1625616@gmail.com>
2023-10-02 12:43:46 +0200

    Add: update exist yaml Add: `update_record` Add: `time_trim_ms`

--- fs_structure_to_yaml.py
+++ fs_structure_to_yaml.py
@@ -20,47 +20,122 @@ import yaml
 import hashlib
 from pathlib import Path
 from docopt import docopt
+from datetime import datetime
+
 
 g_yaml_name = 'index_hash.yaml'
 g_chuck_size = 65536
 g_ignore_linux_hide_files = True
 
+
+def time_trim_ms(t: datetime or float or int):
+    if type(t)==float:
+        return int(t)
+    elif type(t)==datetime:
+        return datetime(t.year, t.month, t.day, t.hour, t.minute, t.second)
+    return t
+
+def update_record(r: dict, data: Path) -> dict:
+    '''
+    type = dir|file|unkown
+    symlink
+    ctime
+    mtime
+
+    md5
+    size
+
+    contents
+    '''
+
+    if data.is_dir():
+        r['type'] = 'dir'
+        if 'size' in r:
+            del r['size']
+        if 'md5' in r:
+            del r['md5']
+    elif data.is_file():
+        r['type'] = 'file'
+        if (r.get('ctime', 0) != time_trim_ms(data.stat().st_ctime) or
+            r.get('mtime', 0) != time_trim_ms(data.stat().st_mtime) or
+            r.get('size', -1) != data.stat().st_size):
+            # if date change - recalc hash
+            r['md5'] = calculate_md5(data)
+        r['size'] = data.stat().st_size
+        if 'contents' in r:
+            del r['contents']
+    else:
+        r['type'] = 'unkown'
+        return r
+
+    if data.is_symlink():
+        r['symlink'] = True
+    else:
+        if 'symlink' in r:
+            del r['symlink']
+
+    r['ctime'] = time_trim_ms(data.stat().st_ctime)
+    r['mtime'] = time_trim_ms(data.stat().st_mtime)
+    return r
+
+
 def create_file_structure(path: Path):
     yaml_path = path / g_yaml_name
+    yaml_loaded = False
 
-    file_structure = {}
+    # Load the existing YAML file if it exists
+    if yaml_path.exists():
+        with open(yaml_path, 'r') as f:
+            try:
+                file_structure = yaml.safe_load(f)
+                yaml_loaded = True
+            except yaml.YAMLError as e:
+                print(f"Error loading existing YAML file {yaml_path}: {e}")
+                return
+    else:
+        file_structure = {}
 
-    # iterate files and collect information
+    # Create a copy of the existing structure to track items to delete
+    items_to_delete = list(file_structure.keys())
+
+    # Iterate files and collect information
     for item in path.iterdir():
-        # TODO: use `igittigitt` library for ignore
+
+        # Use igittigitt library for ignore (implement this if needed)
         if (
            (item.is_dir() and item.name == '.git') or
            (item.name.startswith('.') and g_ignore_linux_hide_files) or
-           (item.is_file() and item.name == g_yaml_name)
+           (item.name == g_yaml_name)
            ):
-            print('IGNORE:', item)
+            print('ingnore:', item)
             continue
 
-        if item.is_dir():
-            file_structure[item.name] = {
-                'type': 'directory',
-                'ctime': item.stat().st_ctime,
-                'mtime': item.stat().st_mtime,
-            }
-        else:
-            file_structure[item.name] = {
-                'type': 'symlink' if item.is_symlink() else 'file',
-                'size': item.stat().st_size,
-                'md5': calculate_md5(item),
-                'ctime': item.stat().st_ctime,
-                'mtime': item.stat().st_mtime,
-            }
+        if item.name in items_to_delete:
+            items_to_delete.remove(item.name)
 
-    if yaml_path.exists():
-        print('SKIP(EXISTS):', str(yaml_path))
+        if not item.name in file_structure:
+            # add
+            print('add:', item.name)
+            file_structure[item.name] = update_record({}, item)
+        else:
+            # update
+            print('update:', item.name)
+            file_structure[item.name] = update_record(file_structure[item.name], item)
+
+
+    # Remove items that were not encountered in the current directory
+    for item_to_delete in items_to_delete:
+        del file_structure[item_to_delete]
+
+
+    if yaml_loaded:
+        #print('SKIP(EXISTS):', str(yaml_path))
+        print('SAVE UPDATED:', str(yaml_path))
     else:
-        print('SAVE:', str(yaml_path))
-        save_to_yaml(file_structure, yaml_path)
+        print('SAVE NEW:', str(yaml_path))
+
+    # Save the updated structure to the YAML file
+    save_to_yaml(file_structure, yaml_path)
 
     # Search directory in list (for recursion)
     for name, content in file_structure.items():
@@ -69,6 +144,7 @@ def create_file_structure(path: Path):
             # Recursion
             create_file_structure(dir_path)
         file_structure[name] = None  # remove item from memory (for optimization)
+
 
 
 def calculate_md5(file_path):
