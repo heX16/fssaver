ab344fa53182845e2a1016aa1a90258f492a474a
heX <hex1625616@gmail.com>
2023-10-24 10:37:15 +0200

    ADD: date time in merged hash file ADD: options --not-add-date ADD: options --file=<yaml_file>

--- fs_struct_merge_to_once_yaml.py
+++ fs_struct_merge_to_once_yaml.py
@@ -2,16 +2,21 @@
 File Structure to YAML
 
 Usage:
-  fs_struct_merge_to_once_yaml.py <start_directory> [<yaml_file>]
+  fs_struct_merge_to_once_yaml.py <start_directory> [--file=<yaml_file>] [--not-add-date]
   fs_struct_merge_to_once_yaml.py -h | --help
 
 Options:
   -h --help     Show this help message and exit.
+  --file=<yaml_file> -f=<yaml_file>  Specify the YAML file name (default: 'index_hash_all.yaml').
+  --not-add-date --nodate            Not add the current date in the YAML file name.
 '''
+
+#TODO: add date to final file
 
 import yaml
 from pathlib import Path
 from docopt import docopt
+import datetime
 
 g_yaml_name = '.index_hash.yaml'
 
@@ -74,12 +79,29 @@ def main():
     arguments = docopt(__doc__)
 
     start_directory = Path(arguments['<start_directory>'])
-    yaml_file = Path(arguments['<yaml_file>'] or 'index_hash_all.yaml')
+    yaml_file = Path(arguments['--file'] or 'index_hash_all.yaml')
 
     merged_structure = merge_contents(start_directory / g_yaml_name)
 
     if not yaml_file.is_absolute():
         yaml_file = start_directory / yaml_file
+
+    # Check if the '--add-date' option is present
+    if arguments['--not-add-date']==False:
+        # Get the current date in the format YYYY-MM-DD
+        current_date = datetime.date.today().isoformat()
+
+        # Split the file name and extension
+        file_name = yaml_file.stem
+        file_extension = yaml_file.suffix
+
+        # Append the current date to the filename
+        new_file_name = f"{file_name}_{current_date}{file_extension}"
+
+        # Create a new Path object with the updated filename
+        yaml_file = yaml_file.with_name(new_file_name)
+
+
 
     save_to_yaml(merged_structure, yaml_file)
     print(f'The merged file structure is saved in {yaml_file}')
